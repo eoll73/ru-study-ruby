3273dcf7e661da0025e0d326bebb22a84a925aef
Victor Vayuta <vittorio073@mail.ru>
2022-10-03 15:15:14 +0300

    first commit

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map! { |i| i.positive? ? array.max : i }
       end
 
       def search(_array, _query)
-        0
+        max = array.length - 1
+        min = 0
+        while min <= max
+          middle_elem = ((max - min) / 2) + min
+          if query < array[middle_elem]
+            max = middle_elem - 1
+          elsif query > array[middle_elem]
+            min = middle_elem + 1
+          else
+            return middle_elem
+          end
+        end
+        -1
       end
     end
   end
