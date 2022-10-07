65a99f062c99d16c29885f8005fcf3bcdf3425a5
Victor Vayuta <vittorio073@mail.ru>
2022-10-07 15:44:16 +0300

    redid everything in the imperative style

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,17 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map! { |i| i.positive? ? array.max : i }
+        max = 0
+
+        array.each { |element|
+            max = element if element > max
+        }
+        (0..array.length - 1).each do |index|
+          array[index] = max if array[index] > 0
+        end
+        array
       end
 
       def search(_array, _query)
-        max = array.length - 1
+        max = _array.length - 1
         min = 0
         while min <= max
           middle_elem = ((max - min) / 2) + min
-          if query < array[middle_elem]
+          if _query < _array[middle_elem]
             max = middle_elem - 1
-          elsif query > array[middle_elem]
+          elsif _query > _array[middle_elem]
             min = middle_elem + 1
           else
             return middle_elem
