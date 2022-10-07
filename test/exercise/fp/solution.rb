d2c48f7cc725224d691139cce67b24da9c5ddff3
Victor Vayuta <vittorio073@mail.ru>
2022-10-07 18:05:47 +0300

    the second task is fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,15 +1,32 @@
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        ratings_of_interest = array.each_with_object([]) do |film, acc|
+          acc << film['rating_kinopoisk'].to_f if rating?(film['rating_kinopoisk'].to_f) && multiple?(film['country'])
+        end
+
+        ratings_of_interest.reduce(0, :+) / ratings_of_interest.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        result = films.map do |film|
+          film['name'].chars.select { |char| char == 'и' }.length if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
+        end.compact
+        result.reduce(:+)
+      end
+
+      private
+
+      def multiple?(string)
+        return false if string.nil?
+
+        string.each_char { |char| return true if char == ',' }
+        false
+      end
+
+      def rating?(value)
+        value != 0
       end
     end
   end
