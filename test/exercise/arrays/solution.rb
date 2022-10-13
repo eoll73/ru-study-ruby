302d7b0d52d053273c6e00e33a1fd21f63e4ec51
Victor Vayuta <vittorio073@mail.ru>
2022-10-13 21:30:39 +0300

    first commit

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,29 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max = 0
+
+        array.each { |element|
+            max = element if element > max
+        }
+        (0..array.length - 1).each do |index|
+          array[index] = max if array[index] > 0
+        end
         array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, first = 0, last = array.size - 1)
+        interval = last - first
+        middle = (last + first) / 2
+        return last if array[last] == query
+        return first if array[first] == query
+        return middle if array[middle] == query
+        
+        if interval > 1
+          return array[middle] < query ? self.search(array, query, middle, last) 
+          : self.search(array, query, first, middle) 
+        end
+        return -1
       end
     end
   end
